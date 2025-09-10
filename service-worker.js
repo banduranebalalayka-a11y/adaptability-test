96f6206538b91afaa4498c716d987b943fdc7917
banduranebalalayka-a11y <banduranebalalayka@gmail.com>
2025-09-10 21:05:51 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,18 @@
-const CACHE_NAME = 'adaptability-test-cache-v1';
-const URLS_TO_CACHE = [
-  '/',
+const CACHE_NAME = 'adaptability-test-v23'; // Version updated to v23 to force update
+const urlsToCache = [
+  '.',
   'index.html',
   'manifest.json',
-  'icons/icon-192.png',
-  'icons/icon-512.png'
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
 ];
 
 self.addEventListener('install', event => {
+  // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
         console.log('Opened cache');
-        return cache.addAll(URLS_TO_CACHE);
+        return cache.addAll(urlsToCache);
       })
   );
 });
@@ -21,12 +21,28 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
+        // Cache hit - return response
         if (response) {
           return response;
         }
         return fetch(event.request);
       }
     )
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
